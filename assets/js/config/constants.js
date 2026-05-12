c977a8e428bc85821a9931dccf70f5c68707b58b
tyler25-blip <andyhank543@gmail.com>
2026-05-12 21:00:27 +0200

    Refine text radii and multi-line spacing further

--- assets/js/config/constants.js
+++ assets/js/config/constants.js
@@ -2,12 +2,12 @@ export const SVG_NS = "http://www.w3.org/2000/svg";
 
 export const RING_CENTER = { x: 800, y: 800 };
 export const RING_RADIUS = 430;
-export const LABEL_RADIUS_SINGLE_OUT = 440;
-export const LABEL_RADIUS_SINGLE_IN = 420;
-export const LABEL_RADIUS_MULTI_L0_OUT = 452;
-export const LABEL_RADIUS_MULTI_L1_OUT = 428;
-export const LABEL_RADIUS_MULTI_L0_IN = 432;
-export const LABEL_RADIUS_MULTI_L1_IN = 408;
+export const LABEL_RADIUS_SINGLE_OUT = 442;
+export const LABEL_RADIUS_SINGLE_IN = 418;
+export const LABEL_RADIUS_MULTI_L0_OUT = 458;
+export const LABEL_RADIUS_MULTI_L1_OUT = 426;
+export const LABEL_RADIUS_MULTI_L0_IN = 434;
+export const LABEL_RADIUS_MULTI_L1_IN = 402;
 
 export const LABEL_RADIUS_SINGLE = 430;
 export const LABEL_RADIUS_OUTER = 443;
